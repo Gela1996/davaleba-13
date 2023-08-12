ca6f1e2274329cd256094f4b264b7cf41658c0e5
gela nikabadze <gela.nikabadze.1@gmail.com>
2023-08-12 17:13:18 +0400

    2 nawili

--- apps.js
+++ apps.js
@@ -1,29 +1,50 @@
 // დავალება 1
 
-let number = prompt('Enter number');
+// let number = prompt('Enter number');
 
-'57103'
+// '57103'
 
-let result = 0;
-for(let i = 0; i < number.length; i++) {
-  result += Number(number[i]);
-}
-number[0] // 5
-result = 5
+// let result = 0;
+// for(let i = 0; i < number.length; i++) {
+  //result += Number(number[i]);
+// }
+// number[0] // 5
+// result = 5
 
-number[1] // 7
-result = 5 + 7 // 12
+// number[1] // 7
+// result = 5 + 7 // 12
 
-number[2] // 1
-result = 12 + 1 // 13
+// number[2] // 1
+// result = 12 + 1 // 13
 
-number[3] // 0
-result = 13 + 0 // 13
+// number[3] // 0
+// result = 13 + 0 // 13
 
-number[4] // 3
-result = 13 + 3 // 16
+// number[4] // 3
+// result = 13 + 3 // 16
 
-alert(result);
+// alert(result);
+
 
 // დავალება 2
 
+let min = Number(prompt('enter min number'));
+let max = Number(prompt('enter max number'));
+let result = '';
+
+min = 5
+max = 14
+
+for (let i = min; i<= max; i++) {
+    if(i % 2 === 0) {
+        result += `${i},`;
+    }
+}
+
+// 1) '6,' 
+// 2) '6,' + '8,' 
+// 3) '8,' + '10,'
+// 4) '10, + '12,'
+// 5) '12, + '14,'
+
+alert(result);

d9e2af0900a9716da92b344f2ce276e4b63663be
gela nikabadze <gela.nikabadze.1@gmail.com>
2023-08-12 16:27:02 +0400

    1 nawili

--- apps.js
+++ apps.js
@@ -1,15 +1,29 @@
-function sumOfDigits(number) {
-    const  numStr = number.toString();
-    let sum = 0;
+// დავალება 1
 
-    for (let i = 0; i < numStr.lenght; i++) {
-       sum += parseInt(numStr[i], 10);
-    }
-    return sum;
+let number = prompt('Enter number');
+
+'57103'
+
+let result = 0;
+for(let i = 0; i < number.length; i++) {
+  result += Number(number[i]);
 }
+number[0] // 5
+result = 5
 
-const number = 57103;
-const result = sumOfDigits(number);
-console.log(result);
+number[1] // 7
+result = 5 + 7 // 12
 
+number[2] // 1
+result = 12 + 1 // 13
+
+number[3] // 0
+result = 13 + 0 // 13
+
+number[4] // 3
+result = 13 + 3 // 16
+
+alert(result);
+
+// დავალება 2
 
